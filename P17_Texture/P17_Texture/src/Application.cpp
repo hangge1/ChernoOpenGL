c29a3016e970dee038c4e54e0224e52d29f8bb2b
hangdada <824362330@qq.com>
2024-06-18 18:03:26 +0800

    [feature] 1、引入stb_image类，用于加载图片 2、引入Texture类，用于存储加载的图片 3、修改shader，使用纹理进行显示矩形 4、顶点属性增加uv坐标

---
+++ P17_Texture/P17_Texture/src/Application.cpp
@@ -0,0 +1,120 @@
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "Renderer.h"
+#include "VertexBufferLayout.h"
+#include "Texture.h"
+
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -2;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    //init glew
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        return -3;
+    }
+
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    GLCall(glEnable(GL_BLEND));
+
+    { //解决在glfwTerminate之前让ib和vb析构
+        float positions[] =
+        {
+           -0.5f, -0.5f, 0.0f, 0.0f,
+            0.5f, -0.5f, 1.0f, 0.0f,
+            0.5f,  0.5f, 1.0f, 1.0f,
+           -0.5f,  0.5f, 0.0f, 1.0f
+        };
+
+        unsigned int indices[] =
+        {
+            0, 1, 2,
+            2, 3, 0
+        };
+
+        IndexBuffer ib(indices, 6);
+        VertexBuffer vb(positions, sizeof(float) * 16);
+ 
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        layout.Push<float>(2);
+
+        VertexArray va;
+        va.AddBuffer(vb, layout);
+
+        float uColor[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+        float increment = 0.01f;
+
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+        //shader.SetUniform4f("u_Color", uColor[0], uColor[1], uColor[2], uColor[3]);
+        shader.SetUniform1i("u_Texture", 0);
+
+        va.UnBind();
+        ib.UnBind();
+        vb.UnBind();
+        shader.UnBind();
+
+        Texture texture("res/textures/logo.jpg");
+        texture.Bind();
+
+        Renderer renderer;
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            renderer.Clear();
+
+            //至少要绑定的基本状态 shader + vao + ibo
+            if (uColor[0] >= 1.0f)
+            {
+                increment = -0.01f;
+            }
+            else if (uColor[0] <= 0.0f)
+            {
+                increment = 0.01f;
+            }
+            uColor[0] += increment;
+
+            renderer.Draw(va, ib, shader);
+            //shader.SetUniform4f("u_Color", uColor[0], uColor[1], uColor[2], uColor[3]);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+    }
+    glfwTerminate();
+    return 0;
+}
---
+++ P17_Texture/P17_Texture/src/Shader.cpp
@@ -0,0 +1,153 @@
+
+#include "Shader.h"
+
+#include "Renderer.h"
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+
+
+
+ShaderProgramSource Shader::ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+
+    enum class ShaderType
+    {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+            {
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos)
+            {
+                type = ShaderType::FRAGMENT;
+            }
+        }
+        else
+        {
+            ss[(int)type] << line << "\n";
+        }
+    }
+
+    return { ss[0].str(), ss[1].str() };
+}
+
+GLuint Shader::CompileShader(unsigned int type, const std::string& source)
+{
+    GLuint shader = glCreateShader(type);
+    const char* src = source.c_str();
+
+    glShaderSource(shader, 1, &src, nullptr);
+    glCompileShader(shader);
+
+    //Error handing
+    GLint result;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        GLint length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(shader, length, &length, message);
+
+        std::string desc = (type == GL_VERTEX_SHADER) ? "vertex shader" : "framgent shader";
+        std::cout << "Failed to compile " << desc << std::endl;
+        std::cout << "Error info: " << message << std::endl;
+
+        glDeleteShader(shader);
+        return 0;
+    }
+
+    return shader;
+}
+
+GLuint Shader::CreateShader(const std::string& vshader, const std::string& fshader)
+{
+    GLuint program = glCreateProgram();
+    GLuint vs = CompileShader(GL_VERTEX_SHADER, vshader);
+    GLuint fs = CompileShader(GL_FRAGMENT_SHADER, fshader);
+
+    GLCall(glAttachShader(program, vs));
+    GLCall(glAttachShader(program, fs));
+    GLCall(glLinkProgram(program));
+    GLCall(glValidateProgram(program));
+
+    GLCall(glDeleteShader(vs));
+    GLCall(glDeleteShader(fs));
+
+    return program;
+}
+
+Shader::Shader(const std::string& filepath)
+	:m_filepath(filepath), m_RendererID(0)
+{
+    ShaderProgramSource source = ParseShader(filepath); //以工作目录为相对路径
+    std::cout << "=================VERTEX==================" << std::endl;
+    std::cout << source.vertexSource << std::endl;
+    std::cout << "=================FRAGMENT==================" << std::endl;
+    std::cout << source.fragmentSource << std::endl;
+
+    m_RendererID = CreateShader(source.vertexSource, source.fragmentSource);
+}
+
+Shader::~Shader()
+{
+    GLCall(glDeleteProgram(m_RendererID));
+}
+
+void Shader::Bind() const
+{
+    GLCall(glUseProgram(m_RendererID));
+}
+
+void Shader::UnBind() const
+{
+    GLCall(glUseProgram(0));
+}
+
+//set uniforms
+void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
+{
+    int location = GetUniformLocation(name);
+    GLCall(glUniform4f(location, v0, v1, v2, v3));
+}
+
+void Shader::SetUniform1f(const std::string& name, float v)
+{
+    int location = GetUniformLocation(name);
+    GLCall(glUniform1f(location, v));
+}
+
+void Shader::SetUniform1i(const std::string& name, int v)
+{
+    int location = GetUniformLocation(name);
+    GLCall(glUniform1i(location, v));
+}
+
+unsigned int Shader::GetUniformLocation(const std::string& name)
+{
+    if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
+    {
+        return m_UniformLocationCache[name];
+    }
+
+    int location = glGetUniformLocation(m_RendererID, name.c_str());
+    ASSERT(location != -1);
+
+    m_UniformLocationCache[name] = location;
+
+    return location;
+}
