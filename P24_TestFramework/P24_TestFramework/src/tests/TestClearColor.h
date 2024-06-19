a57c69eaea1b41aa92152468cf115492257cbf7d
hangdada <824362330@qq.com>
2024-06-19 16:38:23 +0800

    [feature] 1、添加一个测试框架，用于注册各种测试用例 2、提供测试菜单类，用于提供按钮点击

---
+++ P24_TestFramework/P24_TestFramework/src/tests/TestClearColor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Test.h"
+
+#include "glm/glm.hpp"
+
+namespace test {
+
+	class TestClearColor : public Test
+	{
+	public:
+		TestClearColor();
+		~TestClearColor();
+
+		void OnUpdate(float deltaTime) override;
+		void OnRender() override;
+		void OnImGuiRender() override;
+
+	private:
+		glm::vec4 m_ClearColor;
+	};
+}
